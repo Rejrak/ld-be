63ba437b49dfb1d41495d7e3b816e1f357a3ac01
Rejrak <lucchettiroberto95@outlook.com>
2025-03-28 01:20:53 +0100

    Update OAuth2 flow to use password grant and adjust token endpoint URLs

--- design/security.go
+++ design/security.go
@@ -5,10 +5,9 @@ import . "goa.design/goa/v3/dsl"
 var OAuth2 = OAuth2Security("oauth2", func() {
 	Description("OAuth2 flow")
 
-	AuthorizationCodeFlow(
-		"https://keycloak.example.com/realms/myrealm/protocol/openid-connect/auth",
-		"https://keycloak.example.com/realms/myrealm/protocol/openid-connect/token",
-		"",
+	PasswordFlow(
+		"http://localhost:8080/realms/LastingDynamics/protocol/openid-connect/token",
+		"http://localhost:8080/realms/LastingDynamics/protocol/openid-connect/token",
 	)
 
 	Scope("openid", "Access basic profile")
