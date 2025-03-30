f9fe78928b8d368b730ecdc4f5d7f76046d8de66
Rejrak <lucchettiroberto95@outlook.com>
2025-03-30 22:28:39 +0200

    Add exercise_type and exercise_set tables with foreign key constraints

--- internal/database/migrations/000001_init.up.sql
+++ internal/database/migrations/000001_init.up.sql
@@ -37,8 +37,29 @@ CREATE TABLE IF NOT EXISTS exercise (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
     workout_id UUID NOT NULL,
+    exercise_type_id UUID NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     deleted_at TIMESTAMP,
+    FOREIGN KEY (exercise_type_id) REFERENCES exercise_type(id) ON DELETE CASCADE,
     FOREIGN KEY (workout_id) REFERENCES workout(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NO EXISTS exercise_type (
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS exercise_set (
+    id UUID PRIMARY KEY,
+    exercise_id UUID NOT NULL,
+    weight DECIMAL(10, 2) NOT NULL,
+    reps INT NOT NULL,
+    rest_time INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (exercise_id) REFERENCES exercise(id) ON DELETE CASCADE
 );
